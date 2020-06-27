291afcf378d4de51c256428957bab998c4f8ca2d
simsimplay <porisim54@gmail.com>
2020-06-27 14:13:34 +0900

    first git

---
+++ polls/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from polls.models import Question, Choice
+
+# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
