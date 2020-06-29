33f217f00ee07d96a8046cef38bcab6f624b79b0
simsimplay <porisim54@gmail.com>
2020-06-29 09:33:25 +0900

    chapter 4 ex

--- polls/views.py
+++ polls/views.py
@@ -73,3 +73,7 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class MyView(View):
+    
