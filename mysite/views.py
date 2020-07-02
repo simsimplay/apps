a996a5a376eb9668f4773cd6c306ce3828a4058b
simsimplay <porisim54@gmail.com>
2020-07-02 13:11:42 +0900

    chapter3

--- mysite/views.py
+++ mysite/views.py
@@ -7,12 +7,12 @@ class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # context['app_list'] = ['polls', 'books'] 
-        dictVerbose = {}
-        for app in apps.get_app_configs():
-            if 'site-packages' not in app.path:
-                dictVerbose[app.label] = app.verbose_name
-        context['verbose_dict'] = dictVerbose
+            context = super().get_context_data(**kwargs)
+            # context['app_list'] = ['polls', 'books']
+            dictVerbose = {}
+            for app in apps.get_app_configs():
+                if 'site-packages' not in app.path:
+                    dictVerbose[app.label] = app.verbose_name
+            context['verbose_dict'] = dictVerbose
 
-        return context
+            return context
